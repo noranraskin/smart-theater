5fabfe0a14b4191d740f342a93c6486d71780099
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-02-27 13:29:52 +0100

    Save settings in map

--- projector/projector.ino
+++ projector/projector.ino
@@ -1,22 +1,35 @@
 #include <Stepper.h>
 #include "ACS712.h"
-
-
-
-const int steps = 200;
-const int speed = 120;
+#include <map>
 
 #define IN1 12
 #define IN2 14
 #define IN3 27
 #define IN4 26
+#define ACS_1 34
+
+
+struct Settings {
+  static const String steps;
+  static const String speed;
+  static const String weight;
+};
+
+const String Settings::steps = "Stepper: Steps per Revolution";
+const String Settings::speed = "Stepper: Motor Speed";
+const String Settings::weight = "ACS: Low Pass weight";
+
+std::map<String, float> params = {
+  {Settings::steps, 200},
+  {Settings::speed, 120},
+  {Settings::weight, 0.01}
+};
+
 
 int c = 0;
 float value  = 0;
-float weight = 0.1;
 
-Stepper motor(steps, IN1, IN2, IN3, IN4);
-
+Stepper motor(params[Settings::steps], IN1, IN2, IN3, IN4);
 ACS712  ACS(34, 5.0, 4095, 185);
 
 
@@ -43,6 +56,7 @@ void loop()
 {
   //  select sppropriate function
   float mA = ACS.mA_AC_sampling();
+  float weight = params[Settings::weight];
   // float mA = ACS.mA_AC();
   value += weight * (mA - value);  // low pass filtering
   if (c % 10 == 0) {
