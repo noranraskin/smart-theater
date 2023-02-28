5e3d243da0080fb2a6b9b63eb38c7e5df92309e1
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-02-28 16:34:14 +0100

    Add hashing function for NVS settings, because strings were too long

--- projector/prefs.h
+++ projector/prefs.h
@@ -1,8 +1,11 @@
 #include <map>
 #include <nvs.h>
-#include "HomeSpan.h"
+#include <nvs_flash.h>
 
-extern nvs_handle_t nvs;
+using std::hash;
+
+nvs_handle_t nvs;
+const char* setting_ns = "settings";
 
 struct Settings {
   static const String steps;
@@ -26,35 +29,60 @@ std::map<String, float> params = {
   {Settings::acs_off_atv, 76},
 };
 
-void set(String key, float val) {
+String hashString(String str) {
+    hash<std::string> hasher;
+    return String(hasher(std::string(str.c_str())));
+}
+
+int openNS() {
+  esp_err_t err = nvs_open(setting_ns, NVS_READWRITE, &nvs);
+  if (err != ESP_OK) {
+    Serial.println("An Error has occurred while opening NVS namespace 'settings'");
+  }
+  return err;
+}
+
+void updateSetting(String key, float val) {
   if (params.find(key) == params.end()) {
+    Serial.println("Couldn't find parameter...");
     return;
   }
   params[key] = val;
-  if (nvs_set_blob(nvs, key.c_str(), &val, sizeof(float)) != ESP_OK) {
-    WEBLOG("Error occurred while writing to NVS");
-    Serial.println("Error occurred while writing to NVS");
+  if (openNS() != ESP_OK) {
+    return;
+  }
+  esp_err_t err;
+  if (err = nvs_set_blob(nvs, hashString(key).c_str(), &val, sizeof(float)); err != ESP_OK) {
+    Serial.printf("Error occurred while writing to NVS %d\n", err);
+    nvs_close(nvs);
     return;
   }
   if (nvs_commit(nvs) != ESP_OK) {
-    WEBLOG("Couldn't commit to NVS");
     Serial.println("Couldn't commit to NVS");
   }
+  nvs_close(nvs);
 }
 
 void loadSettings() {
-  float value;
+  if (nvs_flash_init() != ESP_OK) {
+    Serial.println("Couldn't init nvs partition");
+  }
+  if (openNS() != ESP_OK) {
+    return;
+  }
+  float value = 0;
   size_t size = sizeof(float);
-  for (const auto& [key, _]: params) {
-    esp_err_t err = nvs_get_blob(nvs, key.c_str(), &value, &size);
+  Serial.println("Loading settings");
+  for (auto const& item: params) {
+    esp_err_t err = nvs_get_blob(nvs, hashString(item.first).c_str(), &value, &size);
     if (err == ESP_ERR_NVS_NOT_FOUND) {
       continue;
     }
     if (err != ESP_OK) {
-      WEBLOG("Error occurred while reading setting from NVS");
       Serial.println("Error occurred while reading setting from NVS");
       continue;
     }
-    params[key] = value;
+    params[item.first] = value;
   }
+  nvs_close(nvs);
 }
--- projector/projector.ino
+++ projector/projector.ino
@@ -18,7 +18,6 @@ float value  = 0;
 Stepper motor(params[Settings::steps], IN1, IN2, IN3, IN4);
 ACS712  ACS(34, 5.0, 4095, 185);
 AsyncWebServer server(80);
-nvs_handle_t nvs;
 
 
 void handleSettings(AsyncWebServerRequest *request) {
@@ -27,8 +26,8 @@ void handleSettings(AsyncWebServerRequest *request) {
     AsyncResponseStream * response = request->beginResponseStream("application/json");
     DynamicJsonDocument doc(256);
     JsonArray obj = doc.to<JsonArray>();
-    for (const auto& [label, _]: params) {
-      obj.add(label);
+    for (auto const& item: params) {
+      obj.add(item.first);
     }
     serializeJson(doc, *response);
     request->send(response);
@@ -42,7 +41,9 @@ void handleSettings(AsyncWebServerRequest *request) {
       return;
     } else if (request->method() == WebRequestMethod::HTTP_POST) {
       if (request->hasArg("value")) {
-        set(url, request->arg("value").toFloat());
+        float val = request->arg("value").toFloat();
+        Serial.printf("Update request with %s and %f\n", url.c_str(), val);
+        updateSetting(url, val);
         request->send(200);
         return;
       }
@@ -63,16 +64,13 @@ void setup() {
   if (!LittleFS.begin()) {
 		WEBLOG("An Error has occurred while mounting FS");
 	}
-  if (!nvs_open("settings", NVS_READWRITE, &nvs)) {
-    WEBLOG("An Error has occurred while opening NVS namespace `settings`");
-  }
-  loadSettings();
   homeSpan.enableOTA();
   homeSpan.enableWebLog(20, "pool.ntp.org", "CET", "logs");
   homeSpan.setHostNameSuffix(""); // use null string for suffix (rather than the HomeSpan device ID)
 	homeSpan.setPortNum(8000);		// change port number for HomeSpan so we can use port 80 for the Web Server
 	homeSpan.setWifiCallback(setupWeb);
 	homeSpan.begin(Category::Bridges, "HomeSpan Projector");
+  loadSettings();
 
   ACS.autoMidPoint();
   ACS.suppressNoise(true);
