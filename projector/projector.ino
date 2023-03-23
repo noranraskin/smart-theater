22284747cb4aa8b415b9388dd10092eead1baf8a
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-03-23 11:54:59 +0100

    Remove ATV homespan device add webserver handles for stepper

--- projector/projector.ino
+++ projector/projector.ino
@@ -1,4 +1,4 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
 #include "ACS712.h"
 #include "prefs.h"
 #include "ESPAsyncWebSrv.h"
@@ -7,21 +7,25 @@
 #include "HomeSpan.h"
 #include "homespan_devices.h"
 
-#define STEPPER1 13
+#define STEPPER1 14
 #define STEPPER2 27
 #define STEPPER3 26
 #define STEPPER4 25
 
 // Only use pins from the first ADC channel for ACS sensors, the second is occupied by WIFI
-#define ACS_1 34
-#define ACS_2 35
+#define ACS 34
 
-const int ON_SWITCH_PIN = 32;
+const int LUM_SENSOR = 35;
+const int ON_SWITCH = 32;
+int counter = 0;
+
+// If current reading is more than that it will say projector is on.
+int mASwitch = 2000;
 
 void turnOn() {
-  digitalWrite(ON_SWITCH_PIN, HIGH);
+  digitalWrite(ON_SWITCH, HIGH);
   delay(100);
-  digitalWrite(ON_SWITCH_PIN, LOW);
+  digitalWrite(ON_SWITCH, LOW);
 }
 
 void turnOff() {
@@ -33,13 +37,20 @@ void turnOff() {
 float mA_value_ATV  = 0;
 float mA_value_PROJECTOR = 0;
 
-Stepper motor(params[Settings::steps], STEPPER1, STEPPER2, STEPPER3, STEPPER4);
-ACS712 ACS_ATV(ACS_1, 5.0, 4095, 185);
-ACS712 ACS_PROJECTOR(ACS_2, 5.0, 4095, 185);
+AccelStepper motor(AccelStepper::FULL4WIRE, STEPPER1, STEPPER2, STEPPER3, STEPPER4);
+int motor_position = 0;
+bool motor_stopped = true;
+ACS712 ACS_PROJECTOR(ACS, 5.0, 4095, 185);
 AsyncWebServer server(80);
 
-AppleTV* appleTV;
 Projector* projector;
+
+void motor_task(void * pvParameters) {
+  for(;;) {
+    motor.run();
+    delay(1);
+  }
+}
 
 void handleSettings(AsyncWebServerRequest *request) {
   String url = request->url();
@@ -65,6 +76,9 @@ void handleSettings(AsyncWebServerRequest *request) {
         float val = request->arg("value").toFloat();
         Serial.printf("Update request with %s and %f\n", url.c_str(), val);
         updateSetting(url, val);
+        if (url == Settings::acceleration) {
+          motor.setAcceleration(val);
+        }
         request->send(200);
         return;
       }
@@ -78,7 +92,7 @@ void handleSensor(AsyncWebServerRequest *request) {
   url.replace("/sensor", "");
   url.replace("/", "");
   if (url == "appleTV") {
-    request->send(200, "application/json", String(mA_value_ATV));
+    request->send(200, "application/json", String(analogRead(LUM_SENSOR)));
     return;
   }
   if (url == "projector") {
@@ -89,9 +103,49 @@ void handleSensor(AsyncWebServerRequest *request) {
   return;
 }
 
+void handleProjector(AsyncWebServerRequest *request) {
+	if (request->hasArg("command")) {
+		String command = request->arg("command");
+		if (command == "On") {
+			turnOn();
+		} else if (command == "Off") {
+      turnOff();
+		}
+		request->send(200, "text/plain", "Stereo turned " + request->arg("command"));
+	} else {
+		request->send(400, "text/plain", "Bad  Request");
+	}
+}
+
+void handleStepper(AsyncWebServerRequest *request) {
+	if (request->hasArg("steps") && request->hasArg("speed")) {
+		if (request->hasArg("direction") && request->arg("direction") == "Stop") {
+			motor.stop();
+      motor.disableOutputs();
+		}
+		long steps = request->arg("steps").toInt();
+		long speed = request->arg("speed").toInt();
+		motor.setMaxSpeed(params[Settings::speed] * speed / 100);
+		if (request->hasArg("direction") && request->arg("direction") == "Up") {
+      motor.enableOutputs();
+      motor.move(steps);
+		} else if (request->hasArg("direction") && request->arg("direction") == "Down") {
+      motor.enableOutputs();
+      motor.move(-steps);
+		} else {
+			motor.stop();
+		}
+		request->send(200, "text/plain", "Motor moved");
+	} else {
+		request->send(400, "text/plain", "Bad Request");
+	}
+}
+
 void setupWeb() {
   server.on("/sensor", handleSensor);
   server.on("/settings", handleSettings);
+  server.on("/projector", handleProjector);
+  server.on("/stepper", handleStepper);
   server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
   server.begin();
 }
@@ -104,7 +158,20 @@ void setup() {
 	}
   loadSettings();
   // Setup pins
-  pinMode(ON_SWITCH_PIN, OUTPUT);
+  pinMode(ON_SWITCH, OUTPUT);
+  pinMode(LUM_SENSOR, INPUT);
+  motor.setMaxSpeed(params[Settings::speed]);
+  motor.setAcceleration(params[Settings::acceleration]);
+  motor.setCurrentPosition(0);
+  xTaskCreatePinnedToCore(
+    motor_task,   // Task function
+    "StepperTask",   // Task name
+    10000,   // Stack size
+    NULL,   // Task parameters
+    1,   // Task priority
+    NULL,   // Task handle
+    1   // Core to run the task on (0 or 1)
+  );
   // Setup homespan
   homeSpan.enableOTA();
   homeSpan.enableWebLog(20, "pool.ntp.org", "CET", "logs");
@@ -120,38 +187,16 @@ void setup() {
   new SpanAccessory();
   new Service::AccessoryInformation();
   new Characteristic::Identify();
-  new Characteristic::Name("Apple TV Monitor");
-  appleTV = new AppleTV();
-
-  new SpanAccessory();
-  new Service::AccessoryInformation();
-  new Characteristic::Identify();
   new Characteristic::Name("Projector");
   projector = new Projector();
 
   // Setup ACS sensors
-  ACS_ATV.autoMidPoint();
   ACS_PROJECTOR.autoMidPoint();
-  ACS_ATV.suppressNoise(true);
   ACS_PROJECTOR.suppressNoise(true);
-  mA_value_ATV = ACS_ATV.mA_AC();  // get initial values
   mA_value_PROJECTOR = ACS_PROJECTOR.mA_AC();
 }
 
 
 void loop() {
   homeSpan.poll();
-  if (mA_value_ATV > params[Settings::acs_on_atv]) {
-    if (projector->state->getVal() != 1) {
-      projector->state->setVal(1);
-      projector->update();
-    }
-  }
-  if (mA_value_ATV < params[Settings::acs_off_atv]) {
-    if (projector->state->getVal() != 0) {
-      projector->state->setVal(0);
-      projector->update();
-    }
-  }
-  Serial.println(mA_value_ATV);
 }
