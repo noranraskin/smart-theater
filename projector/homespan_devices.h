a47dd980418d84a1e745dc1361326eeb18442e75
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-03-23 11:53:01 +0100

    Remove ATV homespan device

--- projector/homespan_devices.h
+++ projector/homespan_devices.h
@@ -1,8 +1,6 @@
 #include "HomeSpan.h"
 
-extern ACS712 ACS_ATV;
 extern ACS712 ACS_PROJECTOR;
-extern float mA_value_ATV;
 void turnOn();
 void turnOff();
 
@@ -19,25 +17,5 @@ struct Projector : Service::LightBulb {
             turnOff();
         }
         return true;
-    }
-};
-
-struct AppleTV:Service::LightSensor {
-    SpanCharacteristic *currentValue = new Characteristic::CurrentAmbientLightLevel(0);
-    int counter = 0;
-
-    void loop() {
-        if (currentValue->timeVal() > 10) {
-            float weight = params[Settings::acs_weight];
-            float mA = ACS_ATV.mA_AC();
-            mA_value_ATV += weight * (mA - mA_value_ATV);  // low pass filtering
-            if (counter % int(params[Settings::hs_update_interval]) == 0) {
-                currentValue->setVal(mA_value_ATV, true);
-                counter = 0;
-            } else {
-                currentValue->setVal(mA_value_ATV, false);
-                counter++;
-            }
-        }
     }
 };
